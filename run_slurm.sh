8ed48f03acb6bbe0f1f73a77b2246ff2b5c948ef
mrkulk <tejasdkulkarni@gmail.com>
2016-01-16 19:27:00 -0500

    updated slurm script

--- run_slurm.sh
+++ run_slurm.sh
@@ -3,10 +3,10 @@
 #SBATCH -n 2
 #SBATCH -p general
 #SBATCH --mem 50000
-#SBATCH -t 1-23:00
-#SBATCH --mail-user=av.saeedi@gmail.com
+#SBATCH -t 5-23:00
+#SBATCH --mail-user=tejask@mit.edu
 
 cd dqn
 /home/tejask/envs/my_root/bin/python pyserver.py &
 cd ..
-./run_gpu montezuma_revenge 'subgoal_'$2'_usedist_'$3 $((5000+$num2)) --seed $1 --subgoal $2  --usedistance $3;
+./run_gpu montezuma_revenge fullrun1 5550 12 false
