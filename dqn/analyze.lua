68ddaa258db62afe7b19f5c90071e6c584cfbe71
Will Whitney <wfwhitney@gmail.com>
2016-02-01 15:23:13 -0500

    minor mods

--- dqn/analyze.lua
+++ dqn/analyze.lua
@@ -1,11 +1,13 @@
 -- analyze subgoal success
 
-expname = 'eps_endt=200000_lr=0.00025_port=9000_usedist=true'
+expname = 'basic2'
 dir = 'logs/' .. expname
 
 finalstats = {}
+finalcount = {}
 for i=3,8 do
 	finalstats[i] = 0
+	finalcount[i] = 0
 end
 
 for f in paths.files(dir) do
@@ -14,9 +16,16 @@ for f in paths.files(dir) do
 		-- print(stats[1])
 		for sid, hitrate in pairs(stats[1]) do
 			finalstats[sid] = finalstats[sid] + stats[1][sid]
+			finalcount[sid] = finalcount[sid] + stats[2][sid]
 		end
 	end
 end
 
-print('Final subgoal stats {key=subgoal-id, value=hitrate:')
+for i=3,8 do
+	if finalstats[i] > 0 then
+		finalstats[i] = finalstats[i]/finalcount[i]
+	end
+end
+
+print('Final subgoal stats {key=subgoal-id, value=% hitrate:')
 print(finalstats)
